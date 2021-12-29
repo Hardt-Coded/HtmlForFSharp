d40cc176cadf6561545a0c6c8fa7134e432eec85
Daniel Hardt <microsoft@hardt-solutions.de>
2021-12-29 19:08:52 +0100

    f# domain tst

---
+++ Domain/Library.fs
@@ -0,0 +1,69 @@
+﻿module Domain
+    
+
+type State =
+    | Default
+    | OpenAngleBracket
+    | ElementName
+    | InsideAttributeList
+    | AttributeName
+    | AfterAttributeName
+    | AfterAttributeEqualSign
+    | AfterOpenDoubleQuote
+    | AfterOpenSingleQuote
+    | AttributeValue
+    | InsideElement
+    | CloseAngleBracket
+    | AfterOpenTagSlash
+    | CloseTagSlash
+    | LitAttributeName
+    | LitAttributeValue
+
+open System
+
+let (|OtherChar|_|) (c:char) =
+    match c with
+    | '\''
+    | '"'
+    | ' '
+    | '.'
+    | '<'
+    | '>'
+    | '/'
+    | '='
+    | '}'
+    | '{'
+    | c when Char.IsControl(c) ->
+        None
+    | Some ()
+    
+
+
+let parse chars state =
+    let check idx c s : struct (State * int)=
+        match s with
+        | Default                 ->
+            match c with
+            | '<'   -> (OpenAngleBracket, idx)
+            | _     -> (Default, idx)
+        | OpenAngleBracket        ->
+            match c with
+            | OtherChar -> (ElementName, idx)
+            | '/'       -> ()
+            | '>'       -> (Default, idx)
+        | ElementName             ->
+        | InsideAttributeList     ->
+        | AttributeName           ->
+        | AfterAttributeName      ->
+        | AfterAttributeEqualSign ->
+        | AfterOpenDoubleQuote    ->
+        | AfterOpenSingleQuote    ->
+        | AttributeValue          ->
+        | InsideElement           ->
+        | CloseAngleBracket       ->
+        | AfterOpenTagSlash       ->
+        | CloseTagSlash           ->
+        | LitAttributeName        ->
+        | LitAttributeValue       ->
+        
+
